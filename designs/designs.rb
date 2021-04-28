5a144f2ddc4789ba32e0a3636e60a768cd94f436
Gerald Bauer <gerald.bauer@gmail.com>
2021-04-28 11:53:07 +0200

    up

---
+++ designs/designs.rb
@@ -0,0 +1,26 @@
+$LOAD_PATH.unshift( "../mooncats/lib" )
+require 'mooncats'
+
+
+rootdir = '../../design.mooncats'
+
+colors = ['331a00', '663300', 'e67300', 'ffb366', 'ff9999' ]
+
+designs = (8..11).to_a +
+          (12..15).to_a
+
+designs.each do |num|
+  ['original', 'v2'].each do |series|
+    puts "#{num}"
+
+    name = '%03d' % num
+    text = File.open( "#{rootdir}/#{series}/#{name}.txt", 'r:utf-8') {|f| f.read }
+
+    design = Mooncats::Design.parse( text )
+    cat = Mooncats::Image.new( design: design, colors: colors )
+    cat.save( "#{rootdir}/i/#{series}-#{name}.png" )
+    cat.zoom(4).save( "#{rootdir}/i/#{series}-#{name}x4.png" )
+  end
+end
+
+puts "bye"
