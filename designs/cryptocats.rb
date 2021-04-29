4f04a87d0c92efe8aaa73a1945b9e334afb786e8
Gerald Bauer <gerald.bauer@gmail.com>
2021-04-29 17:42:40 +0200

    up

---
+++ designs/cryptocats.rb
@@ -0,0 +1,23 @@
+$LOAD_PATH.unshift( "../mooncats/lib" )
+require 'mooncats'
+
+
+rootdir = '../../design.mooncats'
+
+colors = ['331a00', '663300', 'e67300', 'ffb366', 'ff9999', '000000', 'ffffff' ]
+
+designs = [0,1,2,3]
+
+series = 'cryptocats'
+designs.each do |num|
+  puts "#{num}"
+
+  name = '%03d' % num
+  design = Mooncats::Design.read( "#{rootdir}/#{series}/#{name}.txt" )
+
+  cat = Mooncats::Image.new( design: design, colors: colors )
+  cat.save( "#{rootdir}/i/#{series}-#{name}.png" )
+  cat.zoom(4).save( "#{rootdir}/i/#{series}-#{name}x4.png" )
+end
+
+puts "bye"
